e20076b05c39040fa03897c34008cf47158e108f
hochunnn <hochun0122@naver.com>
2017-02-08 16:15:42 +0000

    이미지 주소 // 변경하는것, 메인페이지에서 게시판 넘어가는거, 알뜰방 뷰 찍어내는거 약간 수정

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
     
+    def self.change_slash_into_httpslash
+        
+        a = self.where("product_picture_url like?", "#{"//"}%")
+        
+        a.each do |k|
+           k.product_picture_url =  k.product_picture_url.gsub("//","http://")
+           k.save
+        end
+        
+    end
+    
 end
